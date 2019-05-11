f4d03c3598c05904d3d52618cf4ce6bc2fa54deb
c815549 <msetati@jhb.dvt.co.za>
2019-05-11 15:06:39 +0200

    add profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { DataProvider } from '../../providers/data/data';
 
 @IonicPage()
 @Component({
@@ -14,12 +8,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
+  user: any = {
+    nickname: "Mia Mommy Tee Low",
+    id: 1,
+    rating: "3.5",
+    dob: "1991-02-22",
+    gender: "female",
+    race: "black",
+    bodyType: "slim-thick",
+    location: {
+      address: "123 Arcadia, Pretoria",
+      geo: {
+        lat: -25.950187,
+        lng: 28.998042
+      }
+    }
+  };
+  img: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public dataProvider: DataProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+    this.img = `assets/imgs/users/user${this.user.id}.jpg`;
   }
+
+  getAge(date: string): string {
+    return this.dataProvider.getAgeFromDate(date);
+  }
+
+  getDistance(geo) {
+    return this.dataProvider.getLocationFromGeo(geo);
+  }
+
 
 }
